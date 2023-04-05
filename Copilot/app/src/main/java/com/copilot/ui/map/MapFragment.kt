3d28546dec1cd14eaf0ec2c5c455b26eeaad9fe5
xen <selegakamil@gmail.com>
2023-04-05 20:55:52 +0200

    Add SupportMapFragment, implement onMapReady and remove action bar padding from MainActivity

--- Copilot/app/src/main/java/com/copilot/ui/map/MapFragment.kt
+++ Copilot/app/src/main/java/com/copilot/ui/map/MapFragment.kt
@@ -7,10 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.copilot.R
 import com.copilot.databinding.FragmentMapBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapFragment : Fragment() {
-
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
     private var _binding: FragmentMapBinding? = null
 
     private val binding get() = _binding!!
@@ -26,15 +30,19 @@ class MapFragment : Fragment() {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        mapViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
