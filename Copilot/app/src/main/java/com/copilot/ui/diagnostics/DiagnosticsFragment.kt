7831d301e03b123d3d96344a8a9df749612c1b6c
xzqvac <kawkaradoslaw@gmail.com>
2023-04-13 14:58:33 +0200

    Add diagnostic fragment with recyclerView

--- Copilot/app/src/main/java/com/copilot/ui/diagnostics/DiagnosticsFragment.kt
+++ Copilot/app/src/main/java/com/copilot/ui/diagnostics/DiagnosticsFragment.kt
@@ -7,12 +7,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.copilot.data.model.Information
 import com.copilot.databinding.FragmentDiagnosticsBinding
 
 class DiagnosticsFragment : Fragment() {
 
     private var _binding: FragmentDiagnosticsBinding? = null
     private val binding get() = _binding!!
+    private var mList = ArrayList<Information>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,7 +32,25 @@ class DiagnosticsFragment : Fragment() {
         diagnosticsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val adapter = InformationAdapter(mList)
+
+        binding.recycleViewerOfInfo.apply {
+            this.setHasFixedSize(true)
+            this.adapter = adapter
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        addDatatoList()
+
         return root
+    }
+
+    private fun addDatatoList() {
+        mList.add(Information("Water temperature", "90"))
+        mList.add(Information("Oil temperature", "60"))
+        mList.add(Information("RPM", "900"))
+        mList.add(Information("Fuel consumption", "5l / 100km"))
     }
 
     override fun onDestroyView() {
