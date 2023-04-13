f12d14e73dcd911bfc3369a8bfb0c4e81d44620a
xzqvac <kawkaradoslaw@gmail.com>
2023-04-13 23:38:52 +0200

    Change default string

--- Copilot/app/src/main/java/com/copilot/ui/diagnostics/DiagnosticsViewModel.kt
+++ Copilot/app/src/main/java/com/copilot/ui/diagnostics/DiagnosticsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DiagnosticsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Errors"
     }
     val text: LiveData<String> = _text
 }
