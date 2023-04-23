5d1757e4bd4962af36302827b0d4b5c215419c28
xzqvac <kawkaradoslaw@gmail.com>
2023-04-23 13:45:35 +0200

    Move dummy data from fragment to viewModel

--- Copilot/app/src/main/java/com/copilot/ui/diagnostics/DiagnosticsViewModel.kt
+++ Copilot/app/src/main/java/com/copilot/ui/diagnostics/DiagnosticsViewModel.kt
@@ -3,11 +3,26 @@ package com.copilot.ui.diagnostics
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.copilot.data.model.ErrorMessage
+import com.copilot.data.model.InformationMessage
 
 class DiagnosticsViewModel : ViewModel() {
+    private val dummyDataForInformation = listOf(
+        InformationMessage("Water temperature", "90"),
+        InformationMessage("Oil temperature", "60"),
+        InformationMessage("RPM", "900"),
+        InformationMessage("Fuel consumption", "5l / 100km"))
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Errors"
-    }
-    val text: LiveData<String> = _text
+    private val dummyDataForErrors = listOf(
+        ErrorMessage("Fuel Delivery Error", "P0148"),
+        ErrorMessage("Clutch Position Control Error", "P0810"),
+        ErrorMessage("Exhaust Pressure Sensor Low", "P0472"),
+        ErrorMessage("Control Module Programming Error", "P0602"))
+
+    private val _errorsList = MutableLiveData<List<ErrorMessage>>(dummyDataForErrors)
+    val errorsList: LiveData<List<ErrorMessage>> = _errorsList
+
+    private val _informationList = MutableLiveData<List<InformationMessage>>(dummyDataForInformation)
+    val informationList: LiveData<List<InformationMessage>> = _informationList
+
 }
