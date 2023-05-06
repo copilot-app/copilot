5bb9e18de6f77abe40e33da940c7a55a52bf482d
nebcio <324930@uwr.edu.pl>
2023-05-06 15:14:02 +0200

    add first version of alert dialog

--- Copilot/app/src/main/java/com/copilot/MainActivity.kt
+++ Copilot/app/src/main/java/com/copilot/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.copilot
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -13,6 +16,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+        Toast.makeText(applicationContext,
+            android.R.string.yes, Toast.LENGTH_SHORT).show()
+    }
+    val negativeButtonClick = { dialog: DialogInterface, which: Int ->
+        Toast.makeText(applicationContext,
+            android.R.string.no, Toast.LENGTH_SHORT).show()
+    }
+    val neutralButtonClick = { dialog: DialogInterface, which: Int ->
+        Toast.makeText(applicationContext,
+            "Maybe", Toast.LENGTH_SHORT).show()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,7 +42,48 @@ class MainActivity : AppCompatActivity() {
 
         navView.findViewById<View>(R.id.navigation_dashboard).setOnLongClickListener {
             Log.i("MainActivity", "onLongClick: Dashboard")
+            connectedVehiclesAlert(it)
             true
         }
+    }
+
+    fun connectedVehiclesAlert(view: View){
+        val builder = AlertDialog.Builder(this)
+
+        with(builder)
+        {
+            setTitle("Your vehicles")
+
+            setPositiveButton("Add", DialogInterface.OnClickListener { dialogInterface: DialogInterface, i: Int ->
+                addVehicleAlert(view)
+            })
+            setNegativeButton(android.R.string.cancel, negativeButtonClick)
+            setNeutralButton("Maybe", neutralButtonClick)
+            show()
+        }
+
+
+    }
+
+    fun addVehicleAlert(view: View){
+        val items = arrayOf("ESP32 [::12]", "ESP32 [98:11:11:11:12:23]", "ESP32 [9876::12234]", "ESP32 [9876::12234]",
+        "ESP32 [9876::12234]", "ESP32 [9876::12234]", "ESP32 [9876::12234]", "ESP32 [9876::12234]", "ESP32 [9876::12234]",
+        "ESP32 [9876::12234]", "ESP32 [9876::12234]", "ESP32 [1]", "ESP32 [1]", "ESP32 [2]")
+        val builder = AlertDialog.Builder(this)
+
+        with(builder)
+        {
+            setTitle("New vehicle")
+            setItems(items) { dialog, which ->
+                Toast.makeText(applicationContext, items[which] + " is clicked", Toast.LENGTH_SHORT).show()
+            }
+            setPositiveButton("Add", DialogInterface.OnClickListener { dialogInterface: DialogInterface, i: Int ->
+
+            })
+            setNegativeButton(android.R.string.cancel, negativeButtonClick)
+            show()
+        }
+
+
     }
 }
