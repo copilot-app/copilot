ebb35cc54639a27ac5d58bc4209f7165afd3a674
xen <selegakamil@gmail.com>
2023-06-09 20:54:29 +0200

    Add bluetoothGattCallback

--- Copilot/app/src/main/java/com/copilot/ui/menu/BluetoothLeService.kt
+++ Copilot/app/src/main/java/com/copilot/ui/menu/BluetoothLeService.kt
@@ -2,6 +2,12 @@ package com.copilot.ui.menu
 
 import android.app.Service
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothProfile
+import android.bluetooth.BluetoothProfile.STATE_CONNECTED
+import android.bluetooth.BluetoothProfile.STATE_DISCONNECTED
+import android.content.Intent
 import android.os.Binder
 import android.util.Log
 
@@ -9,9 +15,10 @@ class BluetoothLeService : Service() {
 
     private val binder = LocalBinder()
     private var bluetoothAdapter: BluetoothAdapter? = null
+    private var connectionState = STATE_DISCONNECTED
 
     inner class LocalBinder : Binder() {
-        fun getService() : BluetoothLeService {
+        fun getService(): BluetoothLeService {
             return this@BluetoothLeService
         }
     }
@@ -19,7 +26,7 @@ class BluetoothLeService : Service() {
     fun initialize(): Boolean {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter == null) {
-            Log.e(TAG,"Unable to obtain a BluetoothAdapter.")
+            Log.e(TAG, "Unable to obtain a BluetoothAdapter.")
             return false
         }
         return true
@@ -30,7 +37,7 @@ class BluetoothLeService : Service() {
             try {
                 val device = adapter.getRemoteDevice(address)
             } catch (exception: java.lang.IllegalArgumentException) {
-                Log.w(TAG,"Device not found with provided address.")
+                Log.w(TAG, "Device not found with provided address.")
                 return false
             }
         } ?: run {
@@ -38,5 +45,27 @@ class BluetoothLeService : Service() {
             return false
         }
         return true
+    }
+
+    private fun broadcastUpdate(action: String) {
+        val intent = Intent(action)
+        sendBroadcast(intent)
+    }
+
+    private val bluetoothGattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+            if (newState == STATE_CONNECTED) {
+                connectionState = STATE_CONNECTED
+                broadcastUpdate(ACTION_GATT_CONNECTED)
+            } else if (newState == STATE_DISCONNECTED) {
+                connectionState = STATE_DISCONNECTED
+                broadcastUpdate(ACTION_GATT_DISCONNECTED)
+            }
+        }
+    }
+
+    companion object {
+        const val ACTION_GATT_CONNECTED = "com.copilot.ACTION_GATT_CONNECTED"
+        const val ACTION_GATT_DISCONNECTED = "com.copilot.ACTION_GATT_DISCONNECTED"
     }
 }
